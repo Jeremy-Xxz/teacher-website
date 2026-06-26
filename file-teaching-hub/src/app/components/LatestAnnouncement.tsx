3b7022fcf824a19a7588c1455889c78585bab4c0
Jeremy-Xxz <jeremy960312@gmail.com>
2026-06-26 11:38:14 +0800

    Update LatestAnnouncement butten

--- file-teaching-hub/src/app/components/LatestAnnouncement.tsx
+++ file-teaching-hub/src/app/components/LatestAnnouncement.tsx
@@ -30,7 +30,7 @@ export default function LatestAnnouncement({ article }: { article: any }) {
         onClick={handleScroll}
         className="bg-white text-slate-900 px-5 py-2 rounded-xl text-xs font-bold hover:bg-slate-100 transition active:scale-[0.97] cursor-pointer shadow-xs"
       >
-        閱讀全文 →
+        閱讀此文 →
       </button>
     </div>
   );
